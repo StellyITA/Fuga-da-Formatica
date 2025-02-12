5980368d21862b8aab963e40ed0edc775c773015
Stelly <stellyita@gmail.com>
2025-02-12 13:35:04 +0100

    fix: reset mess

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -4,14 +4,13 @@ public class Arrow : MonoBehaviour
 {
     [SerializeField] private GameObject _currentPicture;
     [SerializeField] private GameObject _nextPicture;
-    [SerializeField] private AudioSource _footstepsSound;
     [SerializeField] private AudioPlayer _audioPlayer;
 
     public void OnArrowClick()
     {
         _nextPicture.SetActive(true);
 
-        if (_footstepsSound != null)
+        if (_audioPlayer != null)
         {
             _audioPlayer.PlayAudio();
         }

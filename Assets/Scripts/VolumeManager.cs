915b968f8c146c33b88fe0c9d1bb22e8014763ff
Stelly <stellyita@gmail.com>
2025-02-12 20:13:38 +0100

    feat: save player audio preferences

--- Assets/Scripts/VolumeManager.cs
+++ Assets/Scripts/VolumeManager.cs
@@ -1,22 +1,53 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class VolumeManager : MonoBehaviour
 {
     [SerializeField] private AudioMixer _audioMixer;
+    [SerializeField] private Slider _masterVolumeSlider;
+    [SerializeField] private Slider _musicVolumeSlider;
+    [SerializeField] private Slider _sfxVolumeSlider;
 
-    public void SetMasterVolume(float volume)
+    private const string MasterVolumeKey = "masterVolume";
+    private const string MusicVolumeKey = "musicVolume";
+    private const string SfxVolumeKey = "sfxVolume";
+
+    private void Start()
     {
-        _audioMixer.SetFloat("masterVolume", volume);
+        // Load saved volume preferences and set sliders accordingly
+        _masterVolumeSlider.value = PlayerPrefs.GetFloat(MasterVolumeKey, 0.75f); // Default to 0.75
+        _musicVolumeSlider.value = PlayerPrefs.GetFloat(MusicVolumeKey, 0.75f);
+        _sfxVolumeSlider.value = PlayerPrefs.GetFloat(SfxVolumeKey, 0.75f);
+
+        // Apply the volumes to the Audio Mixer
+        SetMasterVolume(_masterVolumeSlider.value);
+        SetMusicVolume(_musicVolumeSlider.value);
+        SetSfxVolume(_sfxVolumeSlider.value);
     }
 
-    public void SetMusicVolume(float volume)
+    public void SetMasterVolume(float value)
     {
-        _audioMixer.SetFloat("musicVolume", volume);
+        _audioMixer.SetFloat(MasterVolumeKey, Mathf.Log10(value) * 20); // Convert linear slider value to decibels
+        PlayerPrefs.SetFloat(MasterVolumeKey, value);
     }
 
-    public void SetSFXVolume(float volume)
+    public void SetMusicVolume(float value)
     {
-        _audioMixer.SetFloat("sfxVolume", volume);
+        _audioMixer.SetFloat(MusicVolumeKey, Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat(MusicVolumeKey, value);
+    }
+
+    public void SetSfxVolume(float value)
+    {
+        _audioMixer.SetFloat(SfxVolumeKey, Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat(SfxVolumeKey, value);
+    }
+
+    private void OnDestroy()
+    {
+        // Ensure preferences are saved when the object is destroyed
+        PlayerPrefs.Save();
     }
 }
+
